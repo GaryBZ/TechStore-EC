0cd1e799a745fd60120d5d2903bd4fecd6cc5aec
GaryBZ <gbarreiro2@utmachala.edu.ec>
2026-06-01 21:10:22 -0500

    Cambios realizados:
    -Logo creados e implementados
    
    -Footer basico creado
    
    Cambios en general de todo el home

--- ts-frontend/src/app/app.routes.ts
+++ ts-frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Componentes } from './pages/products/componentes/componentes';
+import { Perifericos } from './pages/products/perifericos/perifericos';
+import { Monitores } from './pages/products/monitores/monitores';
+import { Laptops } from './pages/products/laptops/laptops';
+import { DetailProduct } from './pages/products/detail-product/detail-product';
+import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Home },
+	{ path: 'componentes', component: Componentes },
+	{ path: 'perifericos', component: Perifericos },
+	{ path: 'monitores', component: Monitores },
+	{ path: 'laptops', component: Laptops },
+	{ path: 'productos/:id', component: DetailProduct },
+	{ path: 'login', component: Login },
+	{ path: 'register', component: Register },
+	{ path: '**', redirectTo: '' },
+];
